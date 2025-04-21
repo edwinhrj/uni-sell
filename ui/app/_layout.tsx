d38352e53a50ca5e0add41929d9bea6d6a8c2231
edwin <12edwinho@gmail.com>
2025-04-21 17:54:01 +0800

    Reapply "google OAuth signin and signout"
    This reverts commit bd61c894e63a153f421fc2fd3cdba5042cbdce5d.

--- ui/app/_layout.tsx
+++ ui/app/_layout.tsx
@@ -1,7 +1,9 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { ClerkProvider } from "@clerk/clerk-expo";
-
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { ClerkLoaded } from "@clerk/clerk-expo";
+import InitialLayout from "./(auth)/InitialLayout";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 if (!publishableKey) {
@@ -10,21 +12,27 @@ if (!publishableKey) {
   )
 }
 
-
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={publishableKey}>
-      <SafeAreaProvider>
-        <SafeAreaView 
-          style={{ 
-            flex: 1, 
-            backgroundColor: "white"
-          }}
-        >
-          <Stack screenOptions={{ headerShown: false }}></Stack>
-        </SafeAreaView>
-      </SafeAreaProvider>
 
+    // wrap everything in ClerkProvider and ClerkLoaded 
+    // (basically calling a login API like firebase auth to handle 
+    // user sessions)
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+          <SafeAreaView 
+            style={{ 
+              flex: 1, 
+              backgroundColor: "white"
+            }}
+          >
+            {/* additinal logic for double checking and routing a user to either 
+            login or home page is in InitialLayout.tsx */}
+            <InitialLayout />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
     </ClerkProvider>
   );
 }
